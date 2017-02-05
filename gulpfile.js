8348ac8ac120153aaf2406597d239f21ab5a9d54
Kit Yevgeniy <Lawn1995@gmail.com>
2017-02-05 20:38:28 +0200

    Livereload, connect added

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,47 @@
+"use strict";
+
 var gulp = require('gulp'),
 	concatCSS = require('gulp-concat'),
 	cleanCSS = require('gulp-clean-css'),
 	rename = require('gulp-rename'),
 	notify = require('gulp-notify'),
-	watch = require('gulp-watch'); 
+	watch = require('gulp-watch'),
+	autoprefixer = require('gulp-autoprefixer'),
+	connect = require('gulp-connect'); 
 
-gulp.task('default', function() {
-  gulp.src('css/*.css')
-  	.pipe(concatCSS("bundle.css"))
-  	.pipe(cleanCSS(''))
-  	.pipe(rename('bundle.min.css'))
-  	.pipe(gulp.dest('app/css/'))
-  	.pipe(notify('Done!'));
+
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true
+  });
 });
 
 
-gulp.task('watch', function() {
-	gulp.watch('css/*.css', ['default'])
+//css watch concat prefix min
+gulp.task('css', function() {
+  gulp.src('css/*.css')
+  	.pipe(concatCSS("bundle.css"))
+  	.pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+  	.pipe(cleanCSS(''))
+  	.pipe(rename('bundle.min.css'))
+  	.pipe(gulp.dest('app/css/'))
+  	.pipe(connect.reload());
+});
+
+gulp.task('html',function(){
+	gulp.src('app/index.html')
+		.pipe(connect.reload());
 })
+
+//watch for css
+gulp.task('watch', function() {
+	gulp.watch('css/*.css', ['css'])
+	gulp.watch('app/index.html',['html'])
+})
+
+gulp.task('default',['connect','html','css','watch'])
