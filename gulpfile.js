fe3a7fa80deda501e79e6c02db0dd50a50414b29
Kit Yevgeniy <Lawn1995@gmail.com>
2017-02-05 17:00:09 +0200

    gulp watch after css added

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp'),
 	concatCSS = require('gulp-concat'),
 	cleanCSS = require('gulp-clean-css'),
-	rename = require('gulp-rename') ;
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify'),
+	watch = require('gulp-watch'); 
 
 gulp.task('default', function() {
   gulp.src('css/*.css')
   	.pipe(concatCSS("bundle.css"))
   	.pipe(cleanCSS(''))
   	.pipe(rename('bundle.min.css'))
-  	.pipe(gulp.dest('app/'));
+  	.pipe(gulp.dest('app/css/'))
+  	.pipe(notify('Done!'));
 });
+
+
+gulp.task('watch', function() {
+	gulp.watch('css/*.css', ['default'])
+})
